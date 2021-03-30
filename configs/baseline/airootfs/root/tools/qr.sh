7c0ad6ae04a0915e5a947a76d271cdd1dd485505
Yu, Li-Yu <afg984@gmail.com>
2021-03-31 02:01:21 +0800

    use path instead of query string

--- configs/baseline/airootfs/root/tools/qr.sh
+++ configs/baseline/airootfs/root/tools/qr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "https://benchmark.afq984.org/submit?result=$(tools/result.sh | base64)" | qrencode -t ANSIUTF8
+echo "https://benchmark.afq984.org/submit/$(tools/result.sh | base64 | tr /+ _-)" | qrencode -t ANSIUTF8
